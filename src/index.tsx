65c2c418b745a6cc16cfa976b054268aca6b4a2c
Rafal Pastuszak <rafal.pastuszak@gmail.com>
2022-06-19 15:01:55 +0100

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import '@fontsource/eb-garamond'
 
 ReactDOM.render(<App />, document.getElementById('root'))
